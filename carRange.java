381fb0c51045c77677fddc7947502edcd48b5542
Ayush Prakash Thaware <90403188+AyuOrniThrONE@users.noreply.github.com>
2021-12-08 00:48:00 +0530

    Update carRange.java

--- carRange.java
+++ carRange.java
@@ -10,6 +10,7 @@ public class carRange {
                 int m=sc.nextInt();
                 int v=sc.nextInt();
                 int ans=0;
+                
                 if (1<=p && p<=5 && 6<=m && m<=56 && 1<=v && v<=100) {
                     ans=(m-(p-1))*v;
                     System.out.println(ans);
